8cf0c7a991edd0df5614c185be2f93a1719f0d42
Katie Burdett <katie.burdett@ao.com>
2021-08-09 15:01:20 +0100

    [KB] Adding hmc visability test to homepage spec file

--- webdriverio-test/test/specs/homepage.js
+++ webdriverio-test/test/specs/homepage.js
@@ -5,5 +5,11 @@ describe('AO.com homepage HMC test', () => {
         browser.url('https:ao.com')
         const title = browser.getTitle()
         expect(title).toHaveText("ao.com | Washing Machines | TVs | Laptops | Delivering Tomorrow")
+    });
+
+    it('Should have the HMC visible', () => {
+        let HMC = $("*[data-tag-section='hmc']");
+        let isDisplayed = elem.isDisplayed();
+        console.log(isDisplayed); // outputs: false
     })
 })
